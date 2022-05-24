bbc1aeeb75136fa8f7d247cf9ad09c7c7df2b5dd
Sarthak Sharma <sarthak.sharma.che20@itbhu.ac.in>
2022-05-24 11:34:11 +0530

    table and other

--- public/js/script.js
+++ public/js/script.js
@@ -10,7 +10,7 @@ Promise.all([
     faceapi.nets.faceRecognitionNet.loadFromUri('/models').catch(err => console.log(err, 'print it')),
     faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
     faceapi.nets.ssdMobilenetv1.loadFromUri('/models') //heavier/accurate version of tiny face detector
-]).then(()=>{
+]).then(() => {
     btn.disabled = false;
 }) /*enable the button here!!!*/
 
@@ -18,7 +18,7 @@ Promise.all([
 
 // function start() {
 //     // document.body.append('Models Loaded')
-    
+
 //     navigator.getUserMedia(
 //         { video:{} },
 //         stream => video.srcObject = stream,
@@ -29,102 +29,150 @@ Promise.all([
 //     console.log('video added')
 //     recognizeFaces()
 // }
-btn.addEventListener('click', ()=>{
+btn.addEventListener('click', () => {
     navigator.getUserMedia(
-        { video:{} },
+        { video: {} },
         stream => video.srcObject = stream,
+        // const stream = video.srcObject;
         err => console.error(err)
     )
+
+
+
+
+
     //video.src = '../videos/speech.mp4'
     console.log('video added')
     //disable the button again here!!!
     btn.disabled = true;
     recognizeFaces()
     // canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height) it is not working here :(
-        // if(document.getElementById('att').clicked == true){
-        //     console.log("kiliked");
-        //     return;
-        // }//////////////////////////////i left here
-    })   
+    // if(document.getElementById('att').clicked == true){
+    //     console.log("kiliked");
+    //     return;
+    // }//////////////////////////////i left here
+})
+
+async function recognizeFaces() {
+
+    const labeledDescriptors = await loadLabeledImages()
+    const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors, 0.7)
+    const canvas = faceapi.createCanvasFromMedia(video)
+    // alert("my name is")
+    // const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+
+    const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displaySize)
+
+
+
+    timeintervall = setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video).withFaceLandmarks().withFaceDescriptors()
+        // console.log(detections, 'by meee')
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+
+        const results = resizedDetections.map((d) => {
+            return faceMatcher.findBestMatch(d.descriptor)
+        })
+        results.forEach((result, i) => {
+            // this is how you can find the name of the attendee/////////////////////////////////////////////////////////////
+            attendance.add(result.label.toString())
+            console.log(attendance)
+            // console.log(result.label)
+            const box = resizedDetections[i].detection.box
+            const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+            drawBox.draw(canvas)
+            // console.log(attendance.size);//////////
+            if (attendance.size.toString() != '0') {
+                document.getElementById("att").style.display = "initial";
+                // console.log("it should show");
+                document.getElementById("btn").style.display = "none";
+            }
+        })
+    }, 2000)
+
+
+    // everything for viewing attendace is programmed here 
+    /////////////////////////////////////////////////
+    document.getElementById("att").onclick = function () {
+        console.log('yeah');
+        document.getElementById("videoInput").style.display = "none";
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+        clearInterval(timeintervall);
+        canvas.style.display = "none";
+        document.getElementById("att").style.display = "none";
+        // how to stop cameraaaaaaaaaaaaaaaaaaaaaaaa
+        // video.pause();
+        // video.src = "";
+        // video.getTracks()[0].stop();
+
+        // document.getElementById("table").classList.remove("hidden");
+        // making the attendace table right here table table table table table table table table table table table table table table 
         
-    async function recognizeFaces(){
-
-        const labeledDescriptors = await loadLabeledImages()
-        const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors, 0.7)
-        const canvas = faceapi.createCanvasFromMedia(video)
-        // alert("my name is")
-        // const canvas = faceapi.createCanvasFromMedia(video)
-        document.body.append(canvas)
-
-        const displaySize = { width: video.width, height: video.height }
-        faceapi.matchDimensions(canvas, displaySize)
+        let table = document.createElement('table');
+        let thead = document.createElement('table');
+        let tbody = document.createElement('tbody');
+        table.appendChild(thead);
+        table.appendChild(tbody);
+        document.getElementById('body').appendChild(table);
         
-       
-
-        timeintervall = setInterval(async () => {
-            const detections = await faceapi.detectAllFaces(video).withFaceLandmarks().withFaceDescriptors()
-            // console.log(detections, 'by meee')
-            const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        let row1 = document.createElement('tr');
+        let heading1 = document.createElement('th');
+        heading1.innerHTML = "Sr. No."
+        let heading2 = document.createElement('th');
+        heading2.innerHTML = "Name of Student";
+        row1.appendChild(heading1);
+        row1.appendChild(heading2);
+        thead.appendChild(row1);
+        attendance = Array.from(attendance);
+        let length = attendance.length;
+        for (let i = 0; i < length ; i++) {
             
-            canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+            let nextrow = document.createElement('tr');
 
-            const results = resizedDetections.map((d) => {
-                return faceMatcher.findBestMatch(d.descriptor)
-            })
-            results.forEach( (result, i) => {
-                // this is how you can find the name of the attendee/////////////////////////////////////////////////////////////
-                attendance.add(result.label.toString())
-                console.log(attendance)
-                // console.log(result.label)
-                const box = resizedDetections[i].detection.box
-                const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
-                drawBox.draw(canvas)
-                // console.log(attendance.size);//////////
-                if(attendance.size.toString() != '0'){
-                    document.getElementById("att").style.display = "initial";
-                    // console.log("it should show");
-                    document.getElementById("btn").style.display = "none";
-                }
-            })
-        },2000)
+            let row_serial_no = document.createElement('td');
+            row_serial_no.innerHTML = i+1;
+            row_serial_no.style.width = 10;
+            let row_name = document.createElement('td');
+            row_name.innerHTML = attendance[i];
+            
+            
+            nextrow.appendChild(row_serial_no);
+            nextrow.appendChild(row_name);
+            tbody.appendChild(nextrow);
 
-
-       // everything for viewing attendace is programmed here 
-        /////////////////////////////////////////////////
-        document.getElementById("att").onclick = function(){
-            console.log('yeah');
-            document.getElementById("videoInput").style.display = "none";
-            canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-            clearInterval(timeintervall);
-            canvas.style.display = "none";
-            document.getElementById("att").style.display = "none";
-// how to stop cameraaaaaaaaaaaaaaaaaaaaaaaa
-            // video.pause();
-            // video.src = "";
-            // video.getTracks()[0].stop();
-
-
-            document.getElementById("table").classList.remove("hidden");
-            // making the attendace table right here 
-            // const table = document.querySelector('#table')
-            // const tblheadingrow = document.querySelector('#headrow')
-            // const tblheading = document.querySelector('#heading')
-            // tblheadingrow.appendChild(tblheading)
-            // table.appendChild(tblheadingrow)
-            // document.body.append(table);
         }
-    }
-     
-         
 
-    
-    function loadLabeledImages() {
-        const labels = ['sarthak', 'Black Widow', 'Captain America', 'Hawkeye' , 'Jim Rhodes', 'Tony Stark', 'Thor', 'Captain Marvel']
-        // const labels = ['Prashant Kumar'] // for WebCam
-        return Promise.all(
-        labels.map(async (label)=>{
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(stream => {
+                window.localStream = stream;
+                window.localStream.getVideoTracks()[0].stop(); 
+
+            })
+
+            .catch((err) => {
+                console.log(err);
+            });
+        // // stop only video
+        // window.x = localStream;
+        // window.localStream.getVideoTracks()[0].stop(); 
+
+    }
+}
+
+
+
+
+function loadLabeledImages() {
+    const labels = ['sarthak', 'Black Widow', 'Captain America', 'Hawkeye', 'Tony Stark', 'Thor', 'Captain Marvel']
+    // const labels = ['Prashant Kumar'] // for WebCam
+    return Promise.all(
+        labels.map(async (label) => {
             const descriptions = []
-            for(let i=1; i<=2; i++) {
+            for (let i = 1; i <= 2; i++) {
                 const img = await faceapi.fetchImage(`../labeled_images/${label}/${i}.jpg`)
                 const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
                 // console.log(label + i + JSON.stringify(detections))
@@ -133,9 +181,11 @@ btn.addEventListener('click', ()=>{
             // document.body.append(label+' Faces Loaded | ')
             return new faceapi.LabeledFaceDescriptors(label, descriptions)
         })
-        )
-    }
-    
-   
+    )
+}
+
+
 // why faceapi is giving error on refreshing?
 // how to stop webcam when it just detect a single face..}
+// prmoise on youtibe
+// rounded corner 
